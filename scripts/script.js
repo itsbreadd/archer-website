ea95285863c796bf4c898dbdc26f85d9cc0a0408
bread <44394029+itsbreadd@users.noreply.github.com>
2019-01-06 07:27:19 +0000

    INDEX PAGE COMPLETE: removed need for media queries on index, added more varibles, implemented jquery for fade in

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,6 @@
-$(document).ready(function () { $('.main-container').hide().fadeIn(3000).delay(6000)});
+/* (document).ready(function () { $('.main-container').hide().fadeIn(3000).delay(6000)
+}); */
+
+$(document).ready(function() {
+    $('.main-container').fadeIn(2500);
+});
