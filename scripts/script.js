56603f7576e2cdc41105bf433edfe333f9daf4b2
bread <44394029+itsbreadd@users.noreply.github.com>
2019-01-18 09:00:28 +0000

    finalised first page, working on navigation menu

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,23 @@
 $(document).ready(function() {
     $('.main-container').fadeIn(2500);
 });
+
+window.onload = function(){
+  if(document.URL.indexOf("education.html") >= 0){
+  document.querySelector(".education").style.backgroundColor = "#5f0081";
+  document.querySelector(".education").style.color = "white";
+    }else if(document.URL.indexOf("interests.html") >= 0){
+      document.querySelector(".interests").style.backgroundColor = "#5f0081"; //DUE TO THIS FUNCTION USING TEXT PRESENT IN THE URL
+      document.querySelector(".interests").style.color = "white";
+      }else{
+        document.querySelector(".contact").style.backgroundColor = "#5f0081"; //IF LAUNCHED INTO LIVE ENVIRONMENT I WILL HAVE TO CHANGE THIS
+        document.querySelector(".education").style.color = "white";
+  }
+}
+
+$(document).ready(function() { //MENU CONTROL
+       $('.btn-mobile').click(function() {
+               $('.menu').slideToggle('slow');
+               $('.downarrow').toggleClass('flip');
+       });
+   });
